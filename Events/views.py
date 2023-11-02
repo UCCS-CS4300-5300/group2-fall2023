f7df0557f8c84f5e6a9a0bf2918b4da70c6177d8
mgibbon2 <mgibbon2@uccs.edu>
2023-11-01 21:06:27 -0600

    WIP crud templates/views update

--- Events/views.py
+++ Events/views.py
@@ -2,9 +2,10 @@
 ### Harvestly
 ### Events Views
 
-from django.views.generic.list import ListView
 from django.shortcuts import render
 from .models import Event
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 class EventsList(ListView):
     """ Get a list of Harvestly events. URL `/get-events-list/` """
@@ -14,7 +15,36 @@ class EventsList(ListView):
 
         # Query events
         model = Event.objects.all()
-        template_name = "events_list.html"
+        template_name = "event_list.html"
 
         # Pass events to events_list.html
         return render(request, template_name, {'eventlist': model})
+    
+class EventDetail(DetailView):
+    def create_event(self, request):
+        model = self
+        template_name = "event_create.html"
+
+        # Pass event to events_create.html
+        return render(request, template_name, {'event': model})
+class EventCreate(CreateView):
+    def update_event(self, request):
+        model = self
+        template_name = "event_update.html"
+
+        # Pass event to events_update.html
+        return render(request, template_name, {'event': model})
+class EventUpdate(UpdateView):
+    def get_event(self, request):
+        model = self
+        template_name = "event_detail.html"
+
+        # Pass event to events_detail.html
+        return render(request, template_name, {'event': model})
+class EventDelete(DeleteView):
+    model = Event
+
+
+
+
+
