c01ae29766264a500800edf04851b494f5bda35d
SamuelAdamson <adamson.samuel10@gmail.com>
2023-11-02 18:55:32 -0600

    Products models updates

--- Products/models.py
+++ Products/models.py
@@ -6,6 +6,8 @@ from django.db import models
 from Events.models import Event
 
 class Vendor(models.Model):
+    """ Vendor model. """
+
     name = models.CharField(max_length=255)
     description = models.TextField()
 
@@ -14,12 +16,14 @@ class Vendor(models.Model):
 
 
 class Product(models.Model):
+    """ Product model. """
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     # image = models.ImageField(upload_to='products/images') # need pillow installed
     product_vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    product_event = models.ForeignKey(Event, on_delete=models.CASCADE, blank=True, null=True)
+    product_event = models.ForeignKey(Event, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.name
