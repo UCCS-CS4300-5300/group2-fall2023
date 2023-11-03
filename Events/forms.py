44df2df34e5b28b1f469a8f13a1ef8fc648283cc
Victor Eckert <115093079+Daveej-Dev@users.noreply.github.com>
2023-11-03 10:05:05 -0600

    Tentative styling for events Create and Update
    Added HTML/CSS to style the create event and update event pages. I went for a centralized look for the form, but I'm not sure yet if this is the way to go. I also tweaked the back button on the Update page to re-route to the details page for the event.

--- Events/forms.py
+++ Events/forms.py
@@ -14,10 +14,10 @@ class EventForm(forms.ModelForm):
             'end_time'
         ]
         widgets = {
-            'name': forms.TextInput(),
-            'location': forms.TextInput(),
-            'start_time': DateTimeInput(format='%Y-%m-%dT%H:%M'),
-            'end_time': DateTimeInput(format='%Y-%m-%dT%H:%M')
+            'name': forms.TextInput(attrs={'class':'', 'style':'width: 300px; border-radius: 3px;'}),
+            'location': forms.TextInput(attrs={'style':'width: 300px; border-radius: 3px;'}),
+            'start_time': DateTimeInput(format='%Y-%m-%dT%H:%M', attrs={'style':'width: 300px; border-radius: 3px;'}),
+            'end_time': DateTimeInput(format='%Y-%m-%dT%H:%M', attrs={'style':'width: 300px; border-radius: 3px;'})
         }
 
 
