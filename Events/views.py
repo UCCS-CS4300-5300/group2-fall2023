fd56187877bc2d8aa0d0b5f9dd8bd8d8820d7197
Victor Eckert <115093079+Daveej-Dev@users.noreply.github.com>
2023-11-02 18:52:46 -0600

    Event CRUD Complete
    Implemented unstyled CRUD functionality for basic Event model.

--- Events/views.py
+++ Events/views.py
@@ -19,24 +19,7 @@ class EventList(ListView):
 
         # Pass events to events_list.html
         return render(request, template_name, {'eventlist': model})
-    
-    """ Post a new event to the list. URL `/event-list/` """
-    def post(self, request):
-        """ Populate a form object from the POST request, then redirect back to URL `/event-list/` """
-        # Populate form from POST
-        form = EventForm(request.POST)
         
-        # Check the validity of the form
-        if request.method == 'POST' and form.is_valid():
-
-            # Save the form contents as an Event model object to the database
-            object = form.save(commit=False)
-            object.save()
-
-            # Redirect to the event list
-            return redirect('events')
-    
-
 class EventDetail(DetailView):
     model = Event
     template_name = "event_detail.html"
@@ -58,10 +41,6 @@ class EventUpdate(UpdateView):
 
     # Establish the target template for use
     template_name = "event_update.html"
-
-    # This works, but it returns the user to the event list as opposed
-    # to the page for the current event
-    success_url = reverse_lazy('events')
         
 class EventDelete(DeleteView):
     # Establish the model type and template name for the generic view
