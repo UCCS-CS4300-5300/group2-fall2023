7e33cb045d0d8a225aeb4dbe08ac50d305bb7b54
Victor Eckert <115093079+Daveej-Dev@users.noreply.github.com>
2023-11-03 11:12:28 -0600

    Simplified and Standardized Forms
    Modified the current form layout to make it more dynamic and flexible, while also standardizing it to match Sam's Form Format.

--- Events/forms.py
+++ Events/forms.py
@@ -14,10 +14,17 @@ class EventForm(forms.ModelForm):
             'end_time'
         ]
         widgets = {
-            'name': forms.TextInput(attrs={'class':'', 'style':'width: 300px; border-radius: 3px;'}),
-            'location': forms.TextInput(attrs={'style':'width: 300px; border-radius: 3px;'}),
-            'start_time': DateTimeInput(format='%Y-%m-%dT%H:%M', attrs={'style':'width: 300px; border-radius: 3px;'}),
-            'end_time': DateTimeInput(format='%Y-%m-%dT%H:%M', attrs={'style':'width: 300px; border-radius: 3px;'})
+            'name': forms.TextInput(),
+            'location': forms.TextInput(),
+            'start_time': DateTimeInput(format='%Y-%m-%dT%H:%M'),
+            'end_time': DateTimeInput(format='%Y-%m-%dT%H:%M')
+        }
+
+        labels = {
+            "name": "Event Name:",
+            "location": "Location:",
+            "start_time": "Start Time:",
+            "end_time": "End Time:",
         }
 
 
