a3566a059c67a0aba10e7d3be985da78607206bb
mgibbon2 <mgibbon2@uccs.edu>
2023-11-03 02:02:24 -0600

    abbreviated month names

--- Events/templatetags/filters.py
+++ Events/templatetags/filters.py
@@ -7,13 +7,14 @@ register = template.Library()
 def format_date_range(start_time, end_time):
     # if days are the same only return the one day
     if start_time.date() == end_time.date():
-        start_date = format(start_time, "F jS, Y")
+        start_date = format(start_time, "M jS, Y")
         return start_date
     else:
         # if years are the same only print year at end else print year in both spots
         if start_time.year == end_time.year:
-            start_date = format(start_time, "F jS")
+            start_date = format(start_time, "M jS")
+            end_date = format(end_time, "M jS, Y")
         else:
-            start_date = format(start_time, "F jS, Y")
-            end_date = format(end_time, "F jS, Y")
+            start_date = format(start_time, "M jS, Y")
+            end_date = format(end_time, "M jS, Y")
         return f"{start_date} - {end_date}"
