e4af461bbcdae6d1f05af23c903a2271957fabf8
SamuelAdamson <adamson.samuel10@gmail.com>
2023-11-07 21:17:48 -0700

    Skeleton for Events models/views tests

--- Events/tests/test_models.py
+++ Events/tests/test_models.py
@@ -1,3 +1,12 @@
 ### CS 4300 Fall 2023 Group 2
 ### Harvestly
 ### Test Events Models
+
+from django.test import TestCase
+from Events.models import Event
+
+
+class EventTests(TestCase):
+    """ Test the Event model """
+
+    # TODO
--- Events/tests/test_views.py
+++ Events/tests/test_views.py
@@ -1,3 +1,35 @@
 ### CS 4300 Fall 2023 Group 2
 ### Harvestly
 ### Test Events Views
+
+from django.test import TestCase
+
+
+class EventListTests(TestCase):
+    """ Test the Event List View """
+
+    # TODO
+
+
+class EventDetailTests(TestCase):
+    """ Test the Event Detail View """
+
+    # TODO
+
+
+class EventCreateTests(TestCase):
+    """ Test the Event Create View """
+
+    # TODO
+
+
+class EventUpdateTests(TestCase):
+    """ Test the Event Update View """
+
+    # TODO
+
+
+class EventDeleteTests(TestCase):
+    """ Test the Event Delete View """
+
+    # TODO
