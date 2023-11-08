2c3b056fe743342669181345259cacfb4c6b6953
eander17 <eander17@uccs.edu>
2023-11-07 22:35:45 -0700

    add event model tests, add clean and save methods to Event model

--- Events/models.py
+++ Events/models.py
@@ -4,6 +4,7 @@
 
 from django.db import models
 from django.urls import reverse
+from django.core.exceptions import ValidationError
 
 # TENTATIVE MODEL CONTENTS
 # ID
@@ -13,6 +14,7 @@ from django.urls import reverse
 # Start Time
 # End Time
 
+
 class Event(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
@@ -20,4 +22,14 @@ class Event(models.Model):
     end_time = models.DateTimeField()
 
     def get_absolute_url(self):
-        return reverse('event-detail', args=[str(self.id)])
+        return reverse("event-detail", args=[str(self.id)])
+
+    def clean(self):
+        # don't allow end_time to be before start_time
+        if self.end_time < self.start_time:
+            raise ValidationError("End time cannot be before start time")
+
+    def save(self, *args, **kwargs):
+        # call clean() before saving
+        self.full_clean()
+        super().save(*args, **kwargs)
