b71bfd3756c1369e7326a24b6201b460ba1accaa
Keegan Shry <keeganshry@gmail.com>
2023-11-14 17:45:16 -0700

    Finished permissions for views

--- Events/views.py
+++ Events/views.py
@@ -2,12 +2,13 @@
 ### Harvestly
 ### Events Views
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
 from Events.models import Event
 from Events.forms import EventForm
 from Events.utils import get_coordinates
@@ -101,6 +102,35 @@ class EventUpdate(LoginRequiredMixin, UpdateView):
     # Establish the target template for use
     template_name = "event_update.html"
 
+    def get(self, request, pk):
+        """ Handle get request to delete event """
+        
+        event = get_object_or_404(Event, pk=pk)
+
+        #Only the Event's organizer can get the form
+        if not request.user.id == event.organizer.id:
+            raise PermissionDenied()
+        
+        form = self.form_class
+        return render(request, self.template_name, {"form": form, "event": event})
+
+
+    def post(self, request, pk):
+        """ Handle post request """
+
+        event = get_object_or_404(Event, pk=pk)
+        form = self.form_class(request.POST)
+
+        if not request.user.id == event.organizer.id:
+            raise PermissionDenied()
+
+        if form.is_valid():
+            form.save()
+            return reverse_lazy("event-detail", kwargs={"pk": pk})
+        else:
+            form = self.form_class
+        return render(request, self.template_name, {"form": form, "event": event})
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs['initial'] = {
@@ -149,6 +179,28 @@ class EventDelete(LoginRequiredMixin, DeleteView):
 
     # Establish the success url to redirect back to the events homepage
     success_url = reverse_lazy('events')
+
+    def get(self, request, pk):
+        """ Handle get request to delete event """
+        
+        event = get_object_or_404(Event, pk=pk)
+
+        if not request.user.id == event.organizer.id:
+            raise PermissionDenied()
+
+        return render(request, self.template_name, {"event": event})
+
+
+    def post(self, request, pk):
+        """ Handle post request """
+
+        event = get_object_or_404(Event, pk=pk)
+
+        if not request.user.id == event.organizer.id:
+            raise PermissionDenied()
+        
+        event.delete()
+        return reverse_lazy("events")
 
     def get_context_data(self, **kwargs):
         """ Update context data """
