e66e82846bbdfbaf34aed96210de9cad129bd565
SamuelAdamson <adamson.samuel10@gmail.com>
2023-11-20 09:31:18 -0700

    Restore .gitignore, Products.forms.py, remove odd file

--- Products/forms.py
+++ Products/forms.py
@@ -8,7 +8,7 @@ from Events.models import Event
 
 
 class ProductForm(forms.ModelForm):
-    """Product upload form information"""
+    """ Product upload form information """
 
     # TODO need to add image upload
 
@@ -23,38 +23,30 @@ class ProductForm(forms.ModelForm):
         ]
 
         widgets = {
-            "name": forms.TextInput(
-                attrs={
-                    "required": "required",
-                    "maxlength": "255",
-                    "placeholder": "Product name (max 255 characters)",
-                }
-            ),
-            "price": forms.NumberInput(
-                attrs={
-                    "required": "required",
-                    "min": "0.01",
-                    "max": "100000.00",
-                    "step": "0.01",
-                    "placeholder": "X.XX",
-                }
-            ),
-            "quantity": forms.NumberInput(
-                attrs={
-                    "required": "required",
-                    "step": "1",
-                    "min": "1",
-                    "placeholder": "Product quantity",
-                }
-            ),
+            "name": forms.TextInput(attrs={
+                "required": "required",
+                "maxlength": "255", 
+                "placeholder": "Product name (max 255 characters)"
+            }),
+            "price": forms.NumberInput(attrs={
+                "required": "required",
+                "min": "0.01",
+                "max": "100000.00",
+                "step": "0.01",
+                "placeholder": "X.XX"
+            }),
+            "quantity": forms.NumberInput(attrs={
+                "required": "required",
+                "step": "1",
+                "min": "1",
+                "placeholder": "Product quantity"
+            }),
             "product_event": forms.Select(),
-            "description": forms.Textarea(
-                attrs={
-                    "required": "required",
-                    "rows": 5,
-                    "Placeholder": "Product description",
-                }
-            ),
+            "description": forms.Textarea(attrs={
+                "required": "required",
+                "rows": 5,
+                "Placeholder": "Product description"
+            }),
         }
 
         labels = {
@@ -65,8 +57,28 @@ class ProductForm(forms.ModelForm):
             "description": "Product description",
         }
 
+        error_messages = {
+            "name": {
+                "required": "All fields are required! Include a name!",
+                "max_length": "Maximum name length exceeded! Name length must not exceed 255 characters!"
+            },
+            "price": {
+                "required": "All fields are required! Include a price!",
+                "min_value": "Price must be at least $0.01!",
+                "max_value": "Price must not exceed $100,000.00!",
+            },
+            "quantity": {
+                "required": "All fields are required! Include a quantity!",
+                "max_value": "Maximum quantity exceeded! Maximum quantity for any one item is 100,000!",
+            },
+            "description": {
+                "required": "All fields are required! Include a description!"
+            },
+        }
+
+
     def clean_quantity(self):
-        """Clean quantity field, ensure it is at least 1"""
+        """ Clean quantity field, ensure it is at least 1 """
 
         quantity = self.cleaned_data.get("quantity")
 
@@ -77,7 +89,7 @@ class ProductForm(forms.ModelForm):
 
 
 class ProductReserveForm(forms.Form):
-    """Product reserve form, for user to reserve a quantity of a product"""
+    """ Product reserve form, for user to reserve a quantity of a product """
 
     reserve_quantity = forms.IntegerField()
 
