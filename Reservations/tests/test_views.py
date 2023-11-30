db1b6f1300d78a189994cf57202ca68c9f1313a5
Victor Eckert <113297346+veckertUCCS@users.noreply.github.com>
2023-11-30 08:11:05 -0700

    Completed Delete View Tests
    Finished test suite for Reservation Delete view and tweaked a couple of parameters for the update tests.

--- Reservations/tests/test_views.py
+++ Reservations/tests/test_views.py
@@ -178,7 +178,7 @@ class ReservationUpdateTests(TestCase):
     def test_reservation_update_at_reverse_lookup(self):
         """ Test that the reservation update view exists at reverse lookup of `reservation-update` """
 
-        response = self.client.get(reverse("reservation-update", args=[self.product_1.id]))
+        response = self.client.get(reverse("reservation-update", args=[self.reservation_1.id]))
 
         self.assertEqual(response.status_code, 200)
 
@@ -197,7 +197,7 @@ class ReservationUpdateTests(TestCase):
         self.assertTemplateUsed(response, "layout.html")
 
     def test_reservation_update_missing_object(self):
-        """ Test reservation create view when `Product` object is missing """
+        """ Test reservation update view when `Reservation` object is missing """
 
         response = self.client.get(reverse("reservation-update", args=["999"]))
 
@@ -252,7 +252,7 @@ class ReservationUpdateTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Reserve quantity must be at least 1!")
 
-    def test_reservation_edit_forbidden_by_different_customer(self):
+    def test_reservation_update_forbidden_by_different_customer(self):
         """ Verify that a non-customer cannot edit another user's reservation """
 
         reservation_2 = Reservation.objects.create(
@@ -272,3 +272,99 @@ class ReservationUpdateTests(TestCase):
 
 class ReservationDeleteTests(TestCase):
     """ Test the Reservation Delete view """
+
+    def setUp(self):
+        """ Create a Product and Reservation and login as a customer """
+
+        product_owner_username = "product_owner"
+        customer_username = "customer"
+        password = "testingpassword"
+
+        self.product_owner = User.objects.create_user(
+            username=product_owner_username,
+            password=password,
+        )
+
+        self.customer = User.objects.create_user(
+            username=customer_username,
+            password=password,
+        )
+
+        self.product_1 = Product.objects.create(
+            id=1,
+            name="Product 1",
+            description="Product 1 Description",
+            price=3.45,
+            quantity=10,
+            owner=self.product_owner
+        )
+
+        self.reservation_1 = Reservation.objects.create(
+            id=1,
+            customer=self.customer,
+            product=self.product_1,
+            quantity=5,
+            price=17.25,
+        )
+
+        self.client.login(username=customer_username, password=password)
+
+    def test_reservation_delete_at_url(self):
+        """ Test that the reservation delete view exists at `/reservations/delete/<int:reservation_id>` """
+
+        response = self.client.get(f"/reservations/edit/{self.reservation_1.id}")
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_reservation_delete_at_reverse_lookup(self):
+        """ Test that the reservation delete view exists at reverse lookup of `reservation-delete` """
+
+        response = self.client.get(reverse("reservation-delete", args=[self.reservation_1.id]))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_reservation_delete_uses_template(self):
+        """ Test that the reservation delete view uses correct template """
+
+        response = self.client.get(reverse("reservation-delete", args=[self.reservation_1.id]))
+
+        self.assertTemplateUsed(response, "reservation_delete.html")
+
+    def test_reservation_delete_uses_layout(self):
+        """ Test that the reservation delete view uses layout """
+
+        response = self.client.get(reverse("reservation-delete", args=[self.reservation_1.id]))
+
+        self.assertTemplateUsed(response, "layout.html")
+
+    def test_reservation_delete_missing_object(self):
+        """ Test reservation delete view when `Reservation` object is missing """
+
+        response = self.client.get(reverse("reservation-delete", args=["999"]))
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_reservation_delete_valid(self):
+        """Test the reservation delete post with a valid object ID"""
+
+        response = self.client.post(reverse("reservation-delete", args=[self.reservation_1.id]))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Reservation.objects.filter(id=self.reservation_1.id).exists())
+
+    def test_reservation_delete_forbidden_by_different_customer(self):
+        """ Verify that a non-customer cannot delete another user's reservation """
+
+        reservation_2 = Reservation.objects.create(
+            id=2,
+            product=self.product_1,
+            quantity=5,
+            price=17.25,
+            customer=User.objects.create_user(
+                username="some_other_user",
+                password="some_other_password_12345"
+            )
+        )
+
+        response = self.client.get(reverse("reservation-update", args=[reservation_2.id]))
+        self.assertEqual(response.status_code, 403)
