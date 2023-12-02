ed07dfc8caddd4bbecabc0269e5d2a6cd6a18d68
Victor Eckert <113297346+veckertUCCS@users.noreply.github.com>
2023-12-02 15:49:09 -0700

    Minor PyLint tweaks to Filter tests
    Made formatting adjustments to test_filters.py according to PyLint suggestions

--- Events/tests/test_filters.py
+++ Events/tests/test_filters.py
@@ -1,11 +1,15 @@
+""" Test Suite for Events Filter functionality"""
+
 from django.test import TestCase
 from django.utils import timezone
-from Events.templatetags.filters import format_date_range
 from django.utils.dateformat import format
-
+from Events.templatetags.filters import format_date_range
 
 class FiltersTestCase(TestCase):
+    """ Test Cases for the Events filters """
+
     def test_same_year_date_range(self):
+        """ Test filter handling when the start/end times are same year """
         start_time = timezone.now()
         end_time = start_time + timezone.timedelta(days=2)
 
@@ -15,6 +19,7 @@ class FiltersTestCase(TestCase):
         self.assertEqual(result, expected_result)
 
     def test_different_year_date_range(self):
+        """ Test filter handling when the start/end times are different years """
         start_time = timezone.now()
         end_time = start_time + timezone.timedelta(days=365)
 
